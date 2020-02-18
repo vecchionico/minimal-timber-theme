b2ea7f40413683d048061d04e750deba2f65a21d
vecchionico <mail@nicovecchio.com>
2020-02-18 13:07:33 +0100

    functions.php comment header

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Minimal Timber Theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  minimal timber Theme  
+ *
+ */
 $composer_autoload = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $composer_autoload ) ) {
 	require_once $composer_autoload;
 	$timber = new Timber\Timber();
-}
+}   
